ad05825db3fe4c77e06c778ed1079896da2cddc4
何力 <heli@21tb.com>
2014-12-04 22:33:57 +0800

    修改解析everNote方法

--- src/main/java/com/jyz/imagetaker/analysisImage/AnalysisEvernote.java
+++ src/main/java/com/jyz/imagetaker/analysisImage/AnalysisEvernote.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import com.evernote.edam.error.EDAMNotFoundException;
 import com.evernote.thrift.TException;
 import com.jyz.imagetaker.Constants;
+
 import org.apache.commons.lang3.StringUtils;
 
 import com.evernote.auth.EvernoteAuth;
@@ -23,13 +24,14 @@ import com.evernote.edam.type.Note;
 import com.evernote.edam.type.NoteSortOrder;
 import com.evernote.edam.type.Notebook;
 import com.evernote.thrift.transport.TTransportException;
+
 import org.apache.log4j.Logger;
 
 public class AnalysisEvernote {
 
     protected Logger logger = Logger.getLogger(AnalysisEvernote.class);
 
-    public static Map<String,String> confMap;
+    public static Map<String,Object> confMap;
 
 	private UserStoreClient userStore;
 	private NoteStoreClient noteStore;
@@ -92,9 +94,9 @@ public class AnalysisEvernote {
 	/**
 	 * 获取并显示用户的笔记列表
 	 */
-	public Map<String, String> listNotes() {
+	public Map<String, Object> listNotes() {
 
-        confMap = new HashMap<String, String>();
+        confMap = new HashMap<String, Object>();
         try {
 		// 列出用户的Notes
 		// 首先，获取一个笔记本的列
@@ -135,34 +137,37 @@ public class AnalysisEvernote {
 		return confMap;
 	}
 
-	private void getDataByString(Map<String, String> map, String content) {
-		String[] str = content.split("<br clear=\"none\"/>");
+	private void getDataByString(Map<String, Object> map, String content) {
+		content=content.replace("***********************************************************", "###");
+		String[] st = content.split("###");
+		String content2=st[2];
+		content2=content2.replace("</div><div>", "<br clear=\"none\"/>");
+		content2=content2.replace("<div>", "");
+		content2=content2.replace("</div>", "");
+		content2=content2.replace("</en-note>", "");
+		String[] str=content2.split("<br clear=\"none\"/>");
 		if (str.length > 0) {
-			for (int i = 9; i < str.length; i++) {
+			for (int i = 1; i < str.length; i++) {
 				String line = str[i];
-				String data = line;
-				data = getStr(line, data);
-				if (StringUtils.isNotEmpty(data)) { 
-					String[] datas = data.split("=");
-					map.put(datas[0].trim(),datas[1].trim());
+				if (StringUtils.isNotEmpty(line)) {
+					String[] datas = line.split("=");
+					if (datas[0].trim().equals("ACCESS_KEY") || datas[0].trim().equals("SECRET_KEY") || datas[0].trim().equals("userCounts") ) {
+						map.put(datas[0].trim(),datas[1].trim());
+					}
+					if (datas[0].trim().contains("userName")) {
+						Map<String, String> userMap=new HashMap<String, String>();
+						String bucketName = str[i+1];
+						String[] bucketNameData = bucketName.split("=");
+						userMap.put("bucketName",bucketNameData[1].trim());
+						String domain = str[i+2];
+						String[] domainData = domain.split("=");
+						userMap.put("domain",domainData[1].trim());
+						map.put(datas[1].trim(),userMap);
+					}
+					
 				}
 			}
 		}
-	}
-
-	private String getStr(String line, String data) {
-		if (line.contains("<div>")) {
-			String[] str2 = line.split("<div>");
-			if (StringUtils.isNotEmpty(str2[0])) {
-				data = str2[0];
-			} else {
-				data = str2[1];
-			}
-		}
-		if (line.contains("</div>")) {
-			data = line.substring(0, line.indexOf("</div>"));
-		}
-		return data;
 	}
 
 }
